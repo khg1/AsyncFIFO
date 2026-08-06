3f9eeb8733fa8727038dd85a551d0f1cc73e3fd2
khg1 <khg9056@nyu.edu>
2026-08-06 18:54:27 -0400

    wrote the code for read and write uvm monitor class.

---
+++ tb/agents/read_monitor.sv
@@ -0,0 +1,31 @@
+class read_monitor#(int DATA_WIDTH = 8) extends uvm_monitor;
+    `uvm_component_param_utils(read_monitor#(DATA_WIDTH))
+
+    virtual read_if#(DATA_WIDTH) vif;  
+    uvm_analysis_port #(read_txn_data#(DATA_WIDTH)) read_item_collect_port;      
+    read_txn_data#(DATA_WIDTH)  read_data_item;
+
+    function new(string name = "read_monitor", uvm_component parent = null);
+        super.new(name, parent);
+        read_item_collect_port = new("read_item_collect_port", this);
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        if(!uvm_config_db#(virtual read_if#(DATA_WIDTH))::get(this, "", "read_vif", vif))
+            `uvm_fatal(get_type_name(), "Not set at top level");
+    endfunction
+
+    task run_phase(uvm_phase phase);
+        bit accept = 0;
+        forever begin
+            @(vif.read_fifo_dut);
+            if(accept)  begin
+                read_data_item = read_txn_data#(DATA_WIDTH)::type_id::create("read_data_item");
+                read_data_item.read_data_out = vif.read_fifo_dut.rdata;
+                read_item_collect_port.write(read_data_item);
+            end
+            accept = vif.read_fifo_dut.rd_en && !vif.read_fifo_dut.rempty;
+        end
+    endtask
+endclass
---
+++ tb/agents/write_monitor.sv
@@ -0,0 +1,30 @@
+class write_monitor#(int DATA_WIDTH = 8) extends uvm_monitor;
+    `uvm_component_param_utils(write_monitor#(DATA_WIDTH))
+
+    virtual write_if #(DATA_WIDTH) vif;
+    uvm_analysis_port #(write_txn_data#(DATA_WIDTH)) write_port;
+    write_txn_data #(DATA_WIDTH) write_data_item;
+
+    function new (string name = "write_monitor", uvm_component parent = null);
+        super.new(name, parent);
+        write_port = new("write_port", this);
+    endfunction
+
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        if(!uvm_config_db#(virtual write_if#(DATA_WIDTH))::get(this, "", "write_vif", vif))
+            `uvm_fatal(get_type_name(), "Failed to get the virtual interface")
+    endfunction
+
+    task run_phase(uvm_phase phase);
+        super.run_phase(phase);
+        forever begin
+            @(vif.write_fifo_dut);
+            if(vif.write_fifo_dut.wr_en && !vif.write_fifo_dut.wfull) begin
+                write_data_item = write_txn_data#(DATA_WIDTH)::type_id::create("write_data_item");
+                write_data_item.write_data_in = vif.write_fifo_dut.wdata;
+                write_port.write(write_data_item);
+            end
+        end
+    endtask
+endclass
