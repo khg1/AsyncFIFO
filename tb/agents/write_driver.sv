c4f037200a0c3f47a728935b57e4dfe4f134da0c
khg1 <khg9056@nyu.edu>
2026-08-07 10:07:58 -0400

    fixed a bug with write stall

--- tb/agents/write_driver.sv
+++ tb/agents/write_driver.sv
@@ -9,7 +9,7 @@ class write_driver #(int DATA_WIDTH = 8) extends uvm_driver #(write_txn#(DATA_WI
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        if(!uvm_config_db#(virtual write_if#(DATA_WIDTH))::get(this, "", "read_if", vif))
+        if(!uvm_config_db#(virtual write_if#(DATA_WIDTH))::get(this, "", "write_if", vif))
             `uvm_fatal(get_type_name(), "error extracting vif")
     endfunction
 
@@ -21,9 +21,9 @@ class write_driver #(int DATA_WIDTH = 8) extends uvm_driver #(write_txn#(DATA_WI
             repeat(req.pre_delay)   @(vif.write_driver);
             if(req.do_write) begin
                 vif.write_driver.wr_en <= 1'b1;
+                vif.write_driver.wdata <= req.wdata;
                 do @(vif.write_driver);
                 while(vif.write_driver.wfull);
-                vif.write_driver.wdata <= req.wdata;
                 vif.write_driver.wr_en <= 1'b0;
             end
             seq_item_port.item_done();
