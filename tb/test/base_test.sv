74332f75c48315919e4c900e7d9ce6b26ec291dd
khg1 <khg9056@nyu.edu>
2026-08-13 15:58:00 -0400

    unparameterized base test.

--- tb/test/base_test.sv
+++ tb/test/base_test.sv
@@ -1,7 +1,7 @@
-class base_test#(int DATA_WIDTH = 8) extends uvm_test;
-    `uvm_component_param_utils(base_test#(DATA_WIDTH))
-    env#(DATA_WIDTH) test_env;
-    fifo_virtual_seq#(DATA_WIDTH) v_seq;
+class base_test extends uvm_test;
+    `uvm_component_utils(base_test)
+    env#(8) test_env;
+    fifo_virtual_seq#(8) v_seq;
 
     function new(string name = "base_test", uvm_component parent = null);
         super.new(name, parent);
@@ -9,15 +9,15 @@ class base_test#(int DATA_WIDTH = 8) extends uvm_test;
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        test_env = env#(DATA_WIDTH)::type_id::create("test_env", this);
+        test_env = env#(8)::type_id::create("test_env", this);
     endfunction
 
-    function void run_phase(uvm_phase phase);
+    task run_phase(uvm_phase phase);
         super.run_phase(phase);
         phase.raise_objection(this);
-        v_seq = fifo_virtual_seq#(DATA_WIDTH)::type_id::create("v_seq");
+        v_seq = fifo_virtual_seq#(8)::type_id::create("v_seq");
         v_seq.start(test_env.v_seqr);
         phase.drop_objection(this);
-    endfunction
+    endtask
 
 endclass
