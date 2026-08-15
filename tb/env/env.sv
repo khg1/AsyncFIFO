2f35da64bc6ed0823b61dffb2ea446c8ff1410bf
khg1 <khg9056@nyu.edu>
2026-08-15 15:50:59 -0400

    Connected analysis ports to coverage component

--- tb/env/env.sv
+++ tb/env/env.sv
@@ -5,6 +5,7 @@ class env#(int DATA_WIDTH = 8, int ADDR_WIDTH = 4) extends uvm_env;
     write_agent#(DATA_WIDTH) w_agent;
     fifo_sequencer#(DATA_WIDTH) v_seqr;
     scoreboard#(DATA_WIDTH) scrb;
+    coverage#(DATA_WIDTH) cov;
     
     function new(string name = "env", uvm_component parent = null);
         super.new(name, parent);
@@ -15,7 +16,8 @@ class env#(int DATA_WIDTH = 8, int ADDR_WIDTH = 4) extends uvm_env;
         r_agent = read_agent#(DATA_WIDTH)::type_id::create("r_agent", this);
         w_agent = write_agent#(DATA_WIDTH)::type_id::create("w_agent", this);
         v_seqr = fifo_sequencer#(DATA_WIDTH)::type_id::create("v_seqr", this);
-        scrb = scoreboard#(DATA_WIDTH)::type_id::create("scrb", this); 
+        scrb = scoreboard#(DATA_WIDTH)::type_id::create("scrb", this);
+        cov = coverage#(DATA_WIDTH)::type_id::create("cov", this); 
     endfunction
 
     function void connect_phase(uvm_phase phase);
@@ -24,6 +26,8 @@ class env#(int DATA_WIDTH = 8, int ADDR_WIDTH = 4) extends uvm_env;
         v_seqr.write_seqr = w_agent.w_sequencer;
         r_agent.r_monitor.read_port.connect(scrb.read_reqs_buff.analysis_export);
         w_agent.w_monitor.write_port.connect(scrb.write_reqs_buff.analysis_export);
+        r_agent.r_driver.read_control_port.connect(cov.read_control_imp);
+        w_agent.w_driver.write_control_port.connect(cov.write_control_imp);
     endfunction
 
 endclass
